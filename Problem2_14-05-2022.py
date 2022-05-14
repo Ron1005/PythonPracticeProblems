760200f409e45ff93a5338d0697f2ff1b44adb52
Ron1005 <rohamhad98@gmail.com>
2022-05-14 12:01:14 +0530

    Adding file Problem2_14-05-2022

---
+++ Problem2_14-05-2022.py
@@ -0,0 +1,14 @@
+#Write a Python program to get the Python version you are using
+
+#My Solution
+import sys
+print(sys.version)
+print(sys.version_info)
+
+print("--------------------------------------------------")
+#Proposed Solution
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
