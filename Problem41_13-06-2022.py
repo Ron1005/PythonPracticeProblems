8ab551d3bded6042cb1bf453171508c2e51b31da
Ron1005 <rohamhad98@gmail.com>
2022-06-14 20:27:15 +0530

    Added Problem Statement in Problem41_13-06-2022

--- Problem41_13-06-2022.py
+++ Problem41_13-06-2022.py
@@ -11,6 +11,14 @@ print(type(15))
 print(type(16.2))
 print(type(9.33333333333333333333))
 
+
+# Create a class named 'Rectangle' with two data members- length and breadth and
+# a method to claculate the area which is 'length*breadth'. The class has three constructors which are :
+# 1 - having no parameter - values of both length and breadth are assigned zero.
+# 2 - having two numbers as parameters - the two numbers are assigned as length and breadth respectively.
+# 3 - having one number as parameter - both length and breadth are assigned that number.
+# Now, create objects of the 'Rectangle' class having none, one and two parameters and print their areas.
+
 #Note : We cannot create multiple paramaterised constructors in python like Java , but in Python we can have optional parameters
 #in constructors
 class Rectangle:
