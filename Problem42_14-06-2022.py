759387b04954c8ff7bf3fe1ba0767a6108081903
Ron1005 <rohamhad98@gmail.com>
2022-06-14 20:27:58 +0530

    Added Problem42_14-06-2022

---
+++ Problem42_14-06-2022.py
@@ -0,0 +1,25 @@
+
+# Suppose you have a Piggie Bank with an initial amount of $50 and you have to add some more amount to it.
+# Create a class 'AddAmount' with a data member named 'amount' with an initial value of $50.
+# Now make two constructors of this class as follows:
+# 1 - without any parameter - no amount will be added to the Piggie Bank
+# 2 - having a parameter which is the amount that will be added to Piggie Bank
+# Create object of the 'AddAmount' class and display the final amount in Piggie Bank.
+
+#We can access global variables inside classes using keyword global
+
+piggiebank = 50
+
+class AddAmount:
+    def __init__(self,amount1=0):
+       global piggiebank
+       piggiebank = piggiebank + amount1
+       print(piggiebank)
+
+
+
+AddAmount()
+AddAmount(20)
+
+
+
